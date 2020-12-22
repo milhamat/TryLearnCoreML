fdfc06f6620fc068b8f600639e3e0486efcd984d
MilhamAt <M477ashiddiq@gmail.com>
2020-12-22 18:16:20 +0700

    conect data image to coreML

--- itsFood?/ViewController.swift
+++ itsFood?/ViewController.swift
@@ -29,9 +29,37 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         if let userPikedImage = info[UIImagePickerController.InfoKey.originalImage.rawValue] as? UIImage {
             videoImageView.image = userPikedImage
+            
+//            let ciimage = CIImage(image: userPikedImage)
+            guard let ciimage = CIImage(image: userPikedImage) else {
+                fatalError("Could not convert into CIImage")
+            }
+            detect(image: ciimage)
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(image: CIImage){
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CoreML Model Failed")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let result = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed process image.")
+            }
+            print(result)
+        }
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do{
+            try handler.perform([request])
+        }catch{
+            print(error)
+        }
+        
     }
 
     @IBAction func cameraButtonPressed(_ sender: UIBarButtonItem) {
