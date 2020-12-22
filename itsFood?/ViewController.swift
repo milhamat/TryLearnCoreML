9e6fcfc9696e962f7e0b207878d87cf222d883a5
MilhamAt <M477ashiddiq@gmail.com>
2020-12-22 17:02:26 +0700

    first init camera access

--- itsFood?/ViewController.swift
+++ itsFood?/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var videoImageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .camera //.photoLibrary
+        
+    }
+    
+    private func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let userPikedImage = info[UIImagePickerController.InfoKey.originalImage.rawValue] as? UIImage {
+            videoImageView.image = userPikedImage
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
     }
 
-
+    @IBAction func cameraButtonPressed(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
 }
 
