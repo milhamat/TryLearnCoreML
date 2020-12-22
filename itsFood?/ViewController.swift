1299c29f014a4fafc26d06c42d52f0add658a73b
MilhamAt <M477ashiddiq@gmail.com>
2020-12-22 15:30:50 +0700

    Initial Commit

---
+++ itsFood?/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  itsFood?
+//
+//  Created by Muhammad Ilham Ashiddiq Tresnawan on 22/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
