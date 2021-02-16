ab60ea41514e77d3cbe96cfd77105d4f3af936d9
milhamat <M477ashiddiq@gmail.com>
2021-02-16 16:41:47 +0700

    fix the problem execute detect func

--- itsFood?/ViewController.swift
+++ itsFood?/ViewController.swift
@@ -13,7 +13,7 @@ import Vision
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var videoImageView: UIImageView!
-    
+    var classificationResults : [VNClassificationObservation] = []
     let imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
@@ -25,11 +25,48 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
     }
     
-    private func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+    func detect(image: CIImage) {
         
-        if let userPikedImage = info[UIImagePickerController.InfoKey.originalImage.rawValue] as? UIImage {
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CoreML Model Failed")
+        }
+        
+        let requestML = VNCoreMLRequest(model: model) { (request, error) in
+            guard let result = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed process image.")
+            }
+//            print(result)
+            if let firstResult = result.first {
+                // result.first is the most precentage of match with image model
+                if firstResult.identifier.contains("hotdog"){
+                    DispatchQueue.main.async {
+                        self.navigationItem.title = "HotDog"
+                        print("its a hotdog")
+                    }
+                }else{
+                    DispatchQueue.main.async {
+                        self.navigationItem.title = "Not HotDog"
+                        print("its not a hotdog")
+                    }
+                }
+            }
+        }
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do{
+            try handler.perform([requestML])
+        }catch{
+            print(error)
+        }
+        
+    }
+
+    internal func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+//        if let userPikedImage = info[UIImagePickerController.InfoKey.originalImage.rawValue] as? UIImage {
+        if let userPikedImage = info[.originalImage] as? UIImage {
             videoImageView.image = userPikedImage
-            
+            imagePicker.dismiss(animated: true, completion: nil)
 //            let ciimage = CIImage(image: userPikedImage)
             guard let ciimage = CIImage(image: userPikedImage) else {
                 fatalError("Could not convert into CIImage")
@@ -37,40 +74,13 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             detect(image: ciimage)
         }
         
-        imagePicker.dismiss(animated: true, completion: nil)
+//        imagePicker.dismiss(animated: true, completion: nil)
     }
     
-    func detect(image: CIImage){
-        
-        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
-            fatalError("Loading CoreML Model Failed")
-        }
-        
-        let request = VNCoreMLRequest(model: model) { (request, error) in
-            guard let result = request.results as? [VNClassificationObservation] else {
-                fatalError("Model failed process image.")
-            }
-//            print(result)
-            if let firstResult = result.first {
-                if firstResult.identifier.contains("hotdog"){
-                    self.navigationItem.title = "HotDog"
-                }else{
-                    self.navigationItem.title = "Not HotDog"
-                }
-            }
-        }
-        let handler = VNImageRequestHandler(ciImage: image)
-        
-        do{
-            try handler.perform([request])
-        }catch{
-            print(error)
-        }
-        
-    }
-
+    
     @IBAction func cameraButtonPressed(_ sender: UIBarButtonItem) {
         present(imagePicker, animated: true, completion: nil)
+        // here the imagePicker will take you into camera capture
     }
     
 }
